39c71ccb46cf6f7f5f6e5e6b18aecab1d30b682f
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2024-08-03 10:32:11 -0300

    feat: lista de usuários finalizada

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello World, ok');
+// lista de usuarios
+const listUsers = document.querySelector('#user');
+;
+let users = [
+    { name: 'Lucas', number: 85987692718 },
+    { name: 'Xavier', number: 85991458965 }
+];
+users.forEach(item => {
+    listUsers.innerHTML += `<option>${item.name}</option>`;
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World, ok');
+// lista de usuarios
+const listUsers = document.querySelector('#user') as HTMLSelectElement;
+
+interface Users {
+  name: string;
+  number: number;
+};
+
+let users: Users[] = [
+  {name: 'Lucas', number: 85987692718},
+  {name: 'Xavier', number: 85991458965}
+];
+
+users.forEach(item => {
+  listUsers.innerHTML += `<option>${item.name}</option>`;
+});
+

befd6d5d4cc7bc034b7e4a80634817dfbf291cf2
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2024-08-05 15:32:35 -0300

    feat: botões de gerar relatório finalizados

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -103,3 +103,44 @@ function render(): void {
   lazAcid.textContent = dataBaseObj.lazAcid.toString();
 }
 render();
+
+// gerar relatório
+const relaCloro = document.querySelector('.c-main__result--relaCloro') as HTMLInputElement;
+
+let unconformities: string = "";
+
+relaCloro.addEventListener('click', ()=> {
+
+  if (dataBaseObj.ref < 0.2) {
+    unconformities += "%0ARefeitório abaixo do padrão"
+  } else if (dataBaseObj.ref > 2) {
+    unconformities += "%0ARefeitório acima do padrão"
+  };
+  
+  if (dataBaseObj.ban < 0.2) {
+    unconformities += "%0ABanheiro abaixo do padrão"
+  } else if (dataBaseObj.ban > 2) {
+    unconformities += "%0ABanheiro acima do padrão"
+  };
+  
+  if (dataBaseObj.barrPena < 0.2) {
+    unconformities += "%0ABarreira Pena abaixo do padrão"
+  } else if (dataBaseObj.barrPena > 2) {
+    unconformities += "%0ABarreira Pena acima do padrão"
+  };
+  
+  if (dataBaseObj.barrVis < 0.2) {
+    unconformities += "%0ABarreira Víscera abaixo do padrão"
+  } else if (dataBaseObj.barrVis > 2) {
+    unconformities += "%0ABarreira Víscera acima do padrão"
+  };
+
+  window.open(`https://api.whatsapp.com/send?&text=*Clora%C3%A7%C3%A3o%20da%20%C3%81gua*%0ARefeit%C3%B3rio:%20${dataBaseObj.ref}%0ABanheiro:%20${dataBaseObj.ban}%0ABarreira%20Pena:%20${dataBaseObj.barrPena}%0ABarreira%20V%C3%ADscera:%20${dataBaseObj.barrVis}%0A%C3%81gua%20Bruta:%20${dataBaseObj.aguaBruta}%0A%0AOBS.:%0A${dataBaseObj.obs}${unconformities}`);
+
+})
+
+const relaQuimicos = document.querySelector('.c-main__result--relaQuimicos') as HTMLInputElement;
+
+relaQuimicos.addEventListener('click', ()=> {
+  window.open(`https://api.whatsapp.com/send?&text=*N%C3%ADvel%20dos%20Qu%C3%ADmicos*%0ALazox%20MC:%20${dataBaseObj.lazox}%20L%0ASalmolaz:%20${dataBaseObj.salmolaz}%20L%0ALaz%20Acid:%20${dataBaseObj.lazAcid}%20L`)
+})
